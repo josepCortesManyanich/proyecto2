c2eb8dd0ed6f9c1658107b650015bf7b1dabf74c
Josep <josepcortes6@gmail.com>
2022-06-26 20:27:58 +0200

    closed #8 product model

---
+++ models/producModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose;
+const productSchema = new Schema({
+    name:{
+        type: String,
+    },
+    class:{
+        Type: String,
+    },
+    price:{
+        type: Number,
+    },
+    cbd:{
+        type: Number,
+    },
+    image:{
+        type: String,
+    },
+    description:{
+        type:String
+    }
+})
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product
