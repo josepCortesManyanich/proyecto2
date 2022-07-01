103d60cfca3527f10833716121d464cd74905c7e
Josep <josepcortes6@gmail.com>
2022-07-01 21:08:55 +0200

    solved problems

--- routes/products.js
+++ routes/products.js
@@ -30,6 +30,10 @@ router.post('/create', isLoggedIn, async(req,res,next) => {
     const {name, family, price, cbd} = req.body;
     const priceParsed = parseInt(price);
     const cbdParsed = parseInt(cbd);
+    if (!name || !family || !price || !cbd) {
+        res.render('product/create-products', { error: 'Please fill all fields to create a product' });
+        return;
+    }
     try {
         const newProduct = {name, family, priceParsed, cbdParsed} 
         const product = await Product.create(newProduct)
@@ -62,6 +66,9 @@ router.post('/edit/:productId',isLoggedIn, async(req,res,next) =>{
     const {name, family, price, cbd, description } = req.body;
     const priceParsed = parseInt(price);
     const cbdParsed = parseInt(cbd);
+    if (!name || !family || !price || !cbd) {
+        res.render('product/edit', { error: 'Please fill all fields to create a product' });
+        return;
     try {
         // const productsfromDB = { name, family, priceParsed, cbdParsed };
         const updatedProduct = await Product.findByIdAndUpdate(productId, { name, family, price: priceParsed, cbd: cbdParsed, description }, { new: true })
