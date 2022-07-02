d7795fdc0ed606ba0f43aa45828bd14a1852ac70
Josep <josepcortes6@gmail.com>
2022-07-02 10:48:17 +0200

    job semi done it

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,8 @@ const router = require('express').Router();
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+router.get('/about', (req,res,next) => {
+  res.render('about')
+})
 
 module.exports = router;
